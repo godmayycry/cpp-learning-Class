d6a233d1186a9de48488c81114f398dec64b0e40
godmayycry <gordeevgleb201005@gmail.com>
2026-05-25 16:06:28 +0300

    1.0

---
+++ CppClass.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Building {
+private:
+	int year;
+	string type, color;
+	float HP_procent;
+
+public:
+
+	Building(int y, string t, float hp, string c) {
+		year = y;
+		type = t;
+		color = c;
+		HP_procent = hp;
+	}
+
+	void get_info() {
+		cout << "Type: " << type << ". Color: " << color << ". Year: " << year << ". HP: " << HP_procent << '%' << endl;
+	}
+};
+
+int main() {
+
+	Building school(1999, "School", 68.4f, "Yellow");
+	school.get_info();
+
+	Building house(2020, "House", 89.6f, "Blue");
+	house.get_info();
+
+	return 0;
+}
